0930a7dd3970a6c8b182fcc3b3e5f5e3ba247f5f
SFassnacht <samantha.fassnacht@gmail.com>
2018-06-17 22:04:22 -0400

    Made Splash and Home activities functional.
    Turned splash and home activity images into drawables. Added these drawables to views. Added Intents to go from Splash to Home after 3 seconds. Added Intents to Home buttons.

--- app/src/main/java/spoon/quantifiers/Home.java
+++ app/src/main/java/spoon/quantifiers/Home.java
@@ -1,7 +1,10 @@
 package spoon.quantifiers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void goToHowToPlay(View view){
+        Intent instructionsIntent = new Intent(this, HowToPlay.class);
+        startActivity(instructionsIntent);
+    }
+    public void goToGameMode(View view){
+        Intent gameModeIntent = new Intent(this, GameMode.class);
+        startActivity(gameModeIntent);
     }
 }
--- app/src/main/java/spoon/quantifiers/Splash.java
+++ app/src/main/java/spoon/quantifiers/Splash.java
@@ -1,5 +1,7 @@
 package spoon.quantifiers;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(Splash.this, Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, 3000);
     }
 }
