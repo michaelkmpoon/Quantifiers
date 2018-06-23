038755317d5a9c944b9ab6475c4721275e004c0c
SFassnacht <samantha.fassnacht@gmail.com>
2018-06-23 13:47:31 -0400

    Finalize Home Screen. Edit Game Modes.

--- app/src/main/java/spoon/quantifiers/ClassicGame.java
+++ app/src/main/java/spoon/quantifiers/ClassicGame.java
@@ -3,11 +3,11 @@ package spoon.quantifiers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Game extends AppCompatActivity {
+public class ClassicGame extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_classic_game);
     }
 }
--- app/src/main/java/spoon/quantifiers/GameMode.java
+++ app/src/main/java/spoon/quantifiers/GameMode.java
@@ -1,7 +1,9 @@
 package spoon.quantifiers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameMode extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class GameMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_mode);
+    }
+
+    public void classicMode(View view){
+        Intent gameIntent = new Intent(GameMode.this, ClassicGame.class);
+        startActivity(gameIntent);
     }
 }
