f195dd9d6edc36468f1e2a55befcbc01672c38cf
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2018-08-06 00:28:55 -0400

    update

--- app/src/main/java/spoon/quantifiers/Quantifiers.java
+++ app/src/main/java/spoon/quantifiers/Quantifiers.java
@@ -12,26 +12,3 @@ public class Quantifiers extends Application {
     }
 }
 
-final class FontsOverride {
-
-    public static void setDefaultFont(Context context,
-                                      String staticTypefaceFieldName, String fontAssetName) {
-        final Typeface regular = Typeface.createFromAsset(context.getAssets(),
-                fontAssetName);
-        replaceFont(staticTypefaceFieldName, regular);
-    }
-
-    protected static void replaceFont(String staticTypefaceFieldName,
-                                      final Typeface newTypeface) {
-        try {
-            final Field staticField = Typeface.class
-                    .getDeclaredField(staticTypefaceFieldName);
-            staticField.setAccessible(true);
-            staticField.set(null, newTypeface);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-}
