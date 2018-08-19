760a641352962aa99f3c660c4f30de2d92dc1810
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2018-08-19 14:41:13 -0400

    Update Spinner View

--- app/src/main/java/spoon/quantifiers/ClassicGame.java
+++ app/src/main/java/spoon/quantifiers/ClassicGame.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 import org.w3c.dom.Text;
 
@@ -30,6 +32,15 @@ public class ClassicGame extends AppCompatActivity {
 
         questionView = findViewById(R.id.question);
         questionNumber = getIntent().getExtras().getInt("qNum", 0);
+        Spinner spinner = (Spinner) findViewById(R.id.numeric_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.numeric_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
         if(AppDatabase.INSTANCE == null) {
             questionsDB = AppDatabase.getInstance(this);
