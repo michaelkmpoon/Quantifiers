a7f6743e4252d84f6e188e91aae7ab036b8681db
SamanthaFassnacht <samantha.fassnacht@gmail.com>
2018-09-05 23:32:00 -0400

    Update how to play

---
+++ app/src/main/java/spoon/quantifiers/DemoQuestion.java
@@ -0,0 +1,13 @@
+package spoon.quantifiers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_question);
+    }
+}
